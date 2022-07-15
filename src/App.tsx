15bbb2ea35e16a6a17d9167a8178b61ef32ce0e2
adrian <nege2002@gmail.com>
2022-07-15 16:53:01 +0200

    Proyecto inicial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import { Navigation } from './routes/Navigation';
+
+function App() {
+  return (
+    <>
+      <Navigation />
+    </>
+  );
+}
+
+export default App;
---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,33 @@
+import { BrowserRouter, Navigate, NavLink, Route, Routes } from 'react-router-dom';
+
+import logo from '../logo.svg';
+
+export const Navigation = () => {
+  return (
+    <BrowserRouter>
+      <div className='main-layout'>
+        <nav>
+          <img src={logo} alt='React logo' />
+          <ul>
+            <li>
+              <NavLink className={ ({ isActive }) => isActive ? 'nav-active' : '' } to='/home'>Home</NavLink>
+            </li>
+            <li>
+              <NavLink className={ ({ isActive }) => isActive ? 'nav-active' : '' } to='/about'>About</NavLink>
+            </li>
+            <li>
+              <NavLink className={ ({ isActive }) => isActive ? 'nav-active' : '' } to='/users'>Users</NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path='home'  element={<h1>Home</h1>} />
+          <Route path='about'  element={<h1>About</h1>} />
+          <Route path='users'  element={<h1>Users</h1>} />
+          <Route path='/*' element={<Navigate replace to='home' />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};

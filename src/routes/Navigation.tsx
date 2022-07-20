b8c8157712b2b8c41535e4532c7f38b3e8cc58d9
adrian <nege2002@gmail.com>
2022-07-21 00:26:46 +0200

    feat: lazy components

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,56 @@
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  NavLink,
+  Routes,
+} from "react-router-dom";
+
+import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
+
+import logo from "../logo.svg"; 
+
+export const Navigation = () => {
+  return (
+    <Router>
+      <div className="main-layout">
+        <nav>
+          <img src={logo} alt="React Logo" />
+          <ul>
+            <li>
+              <NavLink
+                to="/lazy1"
+                className={({ isActive }) => (isActive ? "nav-active" : "")}
+              >
+                Lazy 1
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/lazy2"
+                className={({ isActive }) => (isActive ? "nav-active" : "")}
+              >
+                Lazy 2
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/lazy3"
+                className={({ isActive }) => (isActive ? "nav-active" : "")}
+              >
+                Lazy 3
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/lazy1" element={<LazyPage1 />} />
+          <Route path="/lazy2" element={<LazyPage2 />} />
+          <Route path="/lazy3" element={<LazyPage3 />} />
+          <Route element={<Navigate to="/lazy1" replace />} path="/*" />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
